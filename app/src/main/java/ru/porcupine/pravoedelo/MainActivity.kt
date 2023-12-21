e3630fbfbe07e5a95772c7cbb72c2c42d2301a92
HipsterOK <stero141@gmail.com>
2023-12-22 01:15:06 +0500

    disable dark theme

--- app/src/main/java/ru/porcupine/pravoedelo/MainActivity.kt
+++ app/src/main/java/ru/porcupine/pravoedelo/MainActivity.kt
@@ -2,11 +2,14 @@ package ru.porcupine.pravoedelo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
